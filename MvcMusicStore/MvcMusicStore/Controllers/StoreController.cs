69f99b0f4c0d6465b009cbb45fa13f5df527b69d
Li Xin <1289885769@qq.com>
2017-08-27 21:55:49 +0800

    MVC5高级编程学习

---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string  Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        public string Browse(string genre)
+        {
+
+            string message = HttpUtility.HtmlEncode("Store.Browse,Genre= " + genre);
+            return message;
+        }
+
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
