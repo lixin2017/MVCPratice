4fb1dafe7566f6da63d8e6276341f8a0f28ae1d0
Li Xin <1289885769@qq.com>
2017-09-10 16:33:38 +0800

    update

---
+++ Luna/Luna/LunaPlan/Areas/Member/Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Luna.Areas.Member.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage="必填")]
+        [StringLength(20,MinimumLength=4,ErrorMessage="{2}到{1}个字符")]
+        [Display(Name="用户名")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "必填")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "{2}到{1}个字符")]
+        [Display(Name = "密码")]
+        [DataType(DataType.Password)]
+        public string PassWord { get; set; }
+
+        [Display(Name="记住我")]
+        public bool RememberMe { get; set; }
+    }
+}
